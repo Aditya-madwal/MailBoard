43a8d2a9d5a074b5ec81cb1c0edab4705b6b0ee1
aditya-madwal <madwaladitya@gmail.com>
2025-06-23 00:24:39 +0530

    feat : category apis added

--- models/UserCategory.js
+++ models/UserCategory.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const userCategorySchema = new mongoose.Schema({
     name: { type: String, required: true },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    color: { type: String, enum: ['bg-red-500', 'bg-blue-500', 'bg-green-500', 'bg-yellow-500', 'bg-purple-500', 'bg-pink-500', 'bg-gray-500', 'bg-orange-500'], required: true }
 }, { timestamps: true });
 
 export default mongoose.models.UserCategory ||
